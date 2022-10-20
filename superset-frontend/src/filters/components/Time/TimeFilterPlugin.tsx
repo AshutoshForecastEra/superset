4ef2393b2382a936e585ba848fa1c8cd1ac11c9c
Ashutosh Mishra <ashutosh.mishra@forecastera.com>
2022-10-20 18:37:35 +0530

    initial commit

--- superset-frontend/src/filters/components/Time/TimeFilterPlugin.tsx
+++ superset-frontend/src/filters/components/Time/TimeFilterPlugin.tsx
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import { styled } from '@superset-ui/core';
 import React, { useCallback, useEffect } from 'react';
 import DateFilterControl from 'src/explore/components/controls/DateFilterControl';
